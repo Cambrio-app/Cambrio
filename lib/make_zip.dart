979edd1b590b6c8389295c4d9ad229a354424eb8
Jaden <jaden.lorenc@gmail.com>
2022-01-24 21:43:36 -0700

    make a full epub file with the make_zip class, which allows the user to share it, save it, and use it in their favorite ereader.

---
+++ lib/make_zip.dart
@@ -0,0 +1,229 @@
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:async';
+import 'package:share_plus/share_plus.dart';
+import 'dart:io';
+// import 'package:html/parser.dart'; // consider changing this to the dart xml package since it's actually for xml
+import 'package:xml/xml.dart';
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:archive/archive_io.dart';
+
+class MakeZip {
+  String title = 'wat';
+  late final List<Chapter> chapters;
+  String language;
+  String authorName;
+  String bookId;
+  String authorId;
+
+  MakeZip({this.title='default title', required this.authorName, required this.authorId, required this.bookId, this.language = 'en', List<Chapter>? chapters}) :
+        chapters = chapters ?? <Chapter>[];
+  //this code should go here but I can't make an async constructor, so I'll have to refactor with a factory function later.
+  // directory = (await Directory('${(await _filePath)}/$bookId').create())
+
+  Future get _filePath async {
+    // Application documents directory: /data/user/0/{package_name}/{app_name}
+    final applicationDirectory = await getApplicationDocumentsDirectory();
+    // External storage directory: /storage/emulated/0
+    // final externalDirectory = await getExternalStorageDirectory();
+    // Application temporary directory: /data/user/0/{package_name}/cache
+    // final tempDirectory = await getTemporaryDirectory();
+    return applicationDirectory.path;
+  }
+
+  Future<File> _writeToFile(String file, String fileContents) async {
+    // make or access the path to your new epub
+    final path = (await Directory('${(await _filePath)}/$bookId').create()).path;
+    // Directory('$path/EPUB').create();
+    // Directory('$path/META-INF').create();
+    // debugPrint('$path');
+    
+    // access the file, create the directory if needed, and write to that file.
+    return (await File('$path/$file').create(recursive: true)).writeAsString(fileContents);
+    
+  }
+
+  Future<String> _loadAsset(String path) async {
+    // debugPrint(await rootBundle.toString());
+    return await rootBundle.loadString(path);
+  }
+
+  Future<File> _generateOpf() async {
+    const String specificFile = 'EPUB/package.opf';
+    var document = XmlDocument.parse(await _loadAsset('assets/ex_epub/$specificFile'));
+    document.findAllElements('dc:identifier').first.innerText = bookId;
+    document.findAllElements('dc:title').first.innerText = title;
+    document.findAllElements('dc:creator').first.innerText = authorName;
+    document.findAllElements('dc:language').first.innerText = language;
+    // debugPrint(document.outerXml);
+    // debugPrint(document.rootElement.children[1].outerXml);
+
+    // debugPrint(document.rootElement.getElement('manifest')!.outerXml);
+
+    // go through all the chapters and add them to the spine and the manifest
+    for (var i=0;i<chapters.length;i++){
+      // write the manifest
+      final builder = XmlBuilder();
+      builder.element('item', nest: () {
+        builder.attribute('href', 'chapter${i+1}.xhtml'); // i+1 so that we start on 1
+        builder.attribute('id', 'ch${i+1}');
+        builder.attribute('media-type', "application/xhtml+xml");
+      });
+      document.rootElement.getElement('manifest')!.children.add(builder.buildFragment());
+
+      // now write the spine
+      final spineBuilder = XmlBuilder();
+      spineBuilder.element('itemref', nest: () {
+        spineBuilder.attribute('idref', 'ch${i+1}');
+      });
+      document.rootElement.getElement('spine')!.children.add(spineBuilder.buildFragment());
+    }
+    // debugPrint(document.outerXml);
+    // (get the file), construct a real directory if it's not made already, Write to file
+    return await _writeToFile(specificFile, document.toXmlString());
+  }
+
+  Future<File> _generateNav() async {
+    const String specificFile = 'EPUB/nav.xhtml';
+    var document = XmlDocument.parse(await _loadAsset('assets/ex_epub/$specificFile'));
+    document.findAllElements('title').first.innerText = '$title Table of Contents';
+    // debugPrint(document.outerXml);
+    // debugPrint(document.rootElement.children.toString());
+    // go through all the chapters and add them to the table of contents
+    for (var i=0;i<chapters.length;i++){
+      // write the manifest
+      final builder = XmlBuilder();
+      builder.element('li', nest: () {
+        builder.attribute('id', 'ch${i+1}'); // i+1 so that we start on 1
+        builder.element('a', nest: () {
+          builder.attribute('href', 'chapter${i+1}.xhtml');
+          builder.text(chapters[i].title);
+        });
+      });
+      document.findAllElements('ol').first.children.add(builder.buildFragment());
+    }
+    // debugPrint(document.outerXml);
+    // (get the file), construct a real directory if it's not made already, Write to file
+    return await _writeToFile(specificFile, document.toXmlString());
+  }
+
+  // TODO: Write the functions for generating the other files and then zip them.
+  Future<File> _generateContainer() async {
+    const String specificFile = 'META-INF/container.xml';
+    var document = XmlDocument.parse(await _loadAsset('assets/ex_epub/$specificFile'));
+    // (get the file), construct a real directory if it's not made already, Write to file
+    return await _writeToFile(specificFile, document.toXmlString());
+  }
+
+  Future<File> _generateMimetype() async {
+    const String specificFile = 'mimetype';
+    var pureText = await _loadAsset('assets/ex_epub/$specificFile');
+    // (get the file), construct a real directory if it's not made already, Write to file
+    return await _writeToFile(specificFile, pureText);
+  }
+
+  Future<File> _generateTitlePage() async {
+    const String specificFile = 'EPUB/titlepage.xhtml';
+    var document = XmlDocument.parse(await _loadAsset('assets/ex_epub/$specificFile'));
+    document.findAllElements('h').first.innerText = title;
+    document.findAllElements('p').first.innerText = 'by $authorName';
+    // debugPrint(document.toXmlString());
+    // (get the file), construct a real directory if it's not made already, Write to file
+    return await _writeToFile(specificFile, document.toXmlString());
+  }
+
+  Future<File> _generateChapters() async {
+    // TODO: add pagelist, so that pages display properly in ereaders
+    String template = 'EPUB/titlepage.xhtml';
+    var returnfile = File('assets/ex_epub/$template');
+    for (var i=0;i<chapters.length;i++) {
+      String specificFile = 'EPUB/chapter${i+1}.xhtml'; // +1 to make the chapters start at 1
+      var document = XmlDocument.parse(await _loadAsset('assets/ex_epub/$template'));
+      document.findAllElements('body').first.innerXml = chapters[i].text;
+      debugPrint(document.toXmlString());
+      // (get the file), construct a real directory if it's not made already, Write to file
+      returnfile = await _writeToFile(specificFile, document.toXmlString());
+    }
+    return returnfile;
+  }
+
+  void addChapter(String title, String text){
+    chapters.add(Chapter(title,text));
+  }
+
+  Future<File> makeEpub() async{
+    // pull up the path to where the temporary epub will go
+    // final path = await _extFile;
+
+    // for web, it may be possible to simply pass the files directly from each of these functions, and we can avoid needing dart:io.
+    _generateOpf();
+    _generateNav();
+    _generateContainer();
+    _generateMimetype();
+    _generateTitlePage();
+    _generateChapters();
+    // Zip a directory to out.zip using the zipDirectory convenience method
+    var encoder = ZipFileEncoder();
+    encoder.zipDirectory(Directory('${(await _filePath)}/$bookId'), filename: '${(await _filePath)}/$bookId.epub');
+    // await for (var entity in
+    // Directory('${(await _filePath)}').list(recursive: true, followLinks: false)) {
+    //   print(entity.path);
+    // }
+    File zippedFile = File('${(await _filePath)}/$bookId.epub');
+    //share the file
+    // debugPrint(await modifiedFile.readAsString());
+    // print(modifiedFile.path);
+    // debugPrint(Directory('${(await _filePath)}/goodbook').listSync().toString());
+    Share.shareFiles([zippedFile.path]);
+    return zippedFile;
+  }
+
+  // this version is for if it's necessary to use the html parser instead of the xml parser.
+  // Future<File> make_epub() async{
+  //   debugPrint("it is beginning");
+  //   var document = parse(
+  //       '''<?xml version="1.0" encoding="UTF-8"?>
+  //   <package xmlns="http://www.idpf.org/2007/opf" version="3.0" unique-identifier="uid">
+  //   <metadata xmlns:dc="http://purl.org/dc/elements/1.1/">
+  //   <dc:identifier id="uid">totallyuniqueidentifierrighthere</dc:identifier>
+  //   <dc:title>wonderful title of my beautiful book</dc:title>
+  //   <dc:creator>Jaden</dc:creator>
+  //   <dc:language>en</dc:language>
+  //   <meta property="dcterms:modified">2012-02-27T16:38:35Z</meta>
+  //   </metadata>
+  //   <manifest>
+  //   <item href="titlepage.xhtml" id="ttl" media-type="application/xhtml+xml"/>
+  //   <item href="nav.xhtml" id="nav" media-type="application/xhtml+xml" properties="nav"/>
+  //   </manifest>
+  //   <spine>
+  //   <itemref idref="ttl"/>
+  //   <itemref idref="nav" linear="no"/>
+  //   </spine>
+  //   </package>
+  //       ''');
+  //   // print(document.outerHtml);    // debugPrint(_localPath);
+  //   document.getElementsByTagName('dc\\:title')[0].text = 'ya boi';
+  //   // document.findAllElements('dc:title').first.setAttribute('dc:title','wowowo');
+  //   // print(document.getElementsByTagName('dc\\:title')[0].outerHtml);
+  //   // print(document.findAllElements('dc:title'));
+  //   final file = await _extFile;
+  //   // Write the file
+  //   // print(document.outerHtml);
+  //   File modifiedFile = await file.writeAsString(document.body!.innerHtml);
+  //   // File modifiedFile = await file.writeAsString(document.toXmlString());
+  //   //share the file
+  //   // print(await modifiedFile.readAsString());
+  //   // print(await modifiedFile.readAsString());
+  //   // print(modifiedFile.path);
+  //   Share.shareFiles(['${modifiedFile.path}']);
+  //   return modifiedFile;
+  //   // return 'placeholder for file location';
+  // }
+}
+
+class Chapter {
+  String title;
+  String text;
+
+  Chapter(this.title, this.text);
+}
--- lib/widgets/book.dart
+++ lib/widgets/book.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../make_zip.dart';
 
 class Book extends StatelessWidget {
   const Book({Key? key, required this.title}) : super(key: key);
@@ -6,41 +7,50 @@ class Book extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          // margin: EdgeInsets.all(8),
-          // mainAxisAlignment: MainAxisAlignment.end,
-          Container(
-              width: 100.0, //was 125
-              height: 140.0, //was 170
-              decoration: BoxDecoration(
-                color: Theme.of(context).colorScheme.secondaryVariant,
-                boxShadow: const [
-                  BoxShadow(
-                    color: Colors.black,
-                    offset: Offset(
-                      3, // Move to right 3  horizontally
-                      3, // Move to bottom 3 Vertically
-                    ),
-                  )
-                ],
-              ),
-              padding: const EdgeInsets.all(0),
-              alignment: Alignment.topCenter),
-          SizedBox(height: 2), // Spacing between book tile and text
-          Container(
-            padding: new EdgeInsets.only(left: 10, right: 10),
-            child: SizedBox(
-            width: 100,
-              //align text better with book margins
-              child: Text(title,
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(fontWeight: FontWeight.w500),
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis)),
-          )]);
+    return GestureDetector(
+      child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            // margin: EdgeInsets.all(8),
+            // mainAxisAlignment: MainAxisAlignment.end,
+            Container(
+                width: 100.0, //was 125
+                height: 140.0, //was 170
+                decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.secondaryVariant,
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Colors.black,
+                      offset: Offset(
+                        3, // Move to right 3  horizontally
+                        3, // Move to bottom 3 Vertically
+                      ),
+                    )
+                  ],
+                ),
+                padding: const EdgeInsets.all(0),
+                alignment: Alignment.topCenter),
+            const SizedBox(height: 2), // Spacing between book tile and text
+            Container(
+              padding: const EdgeInsets.only(left: 10, right: 10),
+              child: SizedBox(
+              width: 100,
+                //align text better with book margins
+                child: Text(title,
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(fontWeight: FontWeight.w500),
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis)),
+            )]),
+      onTap: () {
+        final MakeZip epubber = MakeZip(title: 'mmmmm', authorName:'mmmm', authorId:'mm', bookId:'mmmmm');
+        epubber.addChapter('chapter 1', '<p>whatevs</p>');
+        epubber.addChapter('chapter 2', '<p>lots of goood stuff</p>');
+        epubber.addChapter('chapter tree', '<p>the end</p>');
+        epubber.makeEpub();
+      },
+    );
   }
 }
